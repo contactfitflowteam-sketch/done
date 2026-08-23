93b4def0dcd85821800e82a2af61a4cfecc594a5
contactfitflowteam-sketch <contactfitflowteam@gmail.com>
2026-08-23 21:58:07 +0530

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -4,12 +4,11 @@ import { Platform } from 'react-native';
 import { registerWidgetTaskHandler } from 'react-native-android-widget';
 import { widgetTaskHandler } from './src/widgets/widget-task-handler';
 
-// Android Native Widget Background Binding
 if (Platform.OS === 'android') {
   try {
     registerWidgetTaskHandler(widgetTaskHandler);
   } catch (e) {
-    console.warn('[Widget] Task handler registration notice:', e);
+    console.warn('[Widget] Task handler registration error:', e);
   }
 }
 
